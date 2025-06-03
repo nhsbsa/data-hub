d9367cce34d1650f5f864fa98fef12e2f886907f
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2025-06-03 13:47:02 +0100

    Version 3 - complete

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,9 @@
 // ES6 or Vanilla JavaScript
+const toggleBtn = document.getElementById('toggleBtn');
+const input = document.getElementById('password-input');
+
+toggleBtn.addEventListener('click', function () {
+  const isPassword = input.type === 'password';
+  input.type = isPassword ? 'text' : 'password';
+  toggleBtn.textContent = isPassword ? 'Hide' : 'Show';
+});
--- app/views/data-hub/mvp/v3/_routes.js
+++ app/views/data-hub/mvp/v3/_routes.js
@@ -8,4 +8,60 @@ router.get('/product-sign-in', (req, res) => {
     res.render('data-hub/mvp/v3/product-sign-in', { userType });
 });
 
+// Create an account
+
+// Enter your full name
+router.post(/full-name/, (req, res) => {
+    const fullName = req.session.data['full-name'];
+    res.redirect('job-role');
+});
+
+// What is your job role
+router.post(/job-role/, (req, res) => {
+    const jobRole = req.session.data['job-role'];
+    res.redirect('associated-organisation');
+});
+
+// Which organisation are you associated with
+router.post(/associated-organisation/, (req, res) => {
+    const associatedOrganisation = req.session.data['associated-organisation'];
+    res.redirect('your-email');
+});
+
+// Which organisation are you associated with
+router.post(/your-email/, (req, res) => {
+    const associatedOrganisation = req.session.data['associated-organisation'];
+    res.redirect('check-your-answers');
+});
+
+// Enter authentication code
+router.post(/auth-code/, (req, res) => {
+    const authCode = req.session.data['auth-code'];
+    res.redirect('create-password');
+});
+
+// Create a password
+router.post(/create-password/, (req, res) => {
+    const passwordInput = req.session.data['password-input'];
+
+    req.session.data['signedIn'] = 'yes'
+
+    res.redirect('my-account');
+});
+
+// Sign in
+router.post(/sign-in/, (req, res) => {
+    req.session.data['signedIn'] = 'yes'
+    req.session.data['your-email'] = ''
+    res.redirect('my-account');
+});
+
+// Sign out
+router.post(/sign-out/, (req, res) => {
+    req.session.data['signedIn'] = 'no'
+
+    res.redirect('../homepage');
+});
+
+
 module.exports = router;
