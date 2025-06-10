2c8a7b5aca17377a0427b5d8b8d1282aa0704c2b
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2025-06-10 15:50:37 +0100

    post-mvp version 1

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,9 +1,30 @@
 // ES6 or Vanilla JavaScript
-const toggleBtn = document.getElementById('toggleBtn');
-const input = document.getElementById('password-input');
+document.addEventListener('DOMContentLoaded', () => {
 
-toggleBtn.addEventListener('click', function () {
-  const isPassword = input.type === 'password';
-  input.type = isPassword ? 'text' : 'password';
-  toggleBtn.textContent = isPassword ? 'Hide' : 'Show';
+    const toggleBtn = document.getElementById('toggleBtn');
+    const input = document.getElementById('password-input');
+
+    if (toggleBtn && input) {
+        toggleBtn.addEventListener('click', function () {
+            const isPassword = input.type === 'password';
+            input.type = isPassword ? 'text' : 'password';
+            toggleBtn.textContent = isPassword ? 'Hide' : 'Show';
+        });
+    } else {
+        console.log('not on the right page...')
+    }
+
+    const button = document.getElementById('hide-getting-started');
+    const expander = document.getElementById('getting-started-expander');
+    const className = 'nhsuk-expander-hidden';
+
+    if (button && expander) {
+        button.addEventListener('click', () => {
+            expander.classList.toggle(className);
+        });
+    } else {
+        console.log("wont work here...");
+    }
+
+
 });
