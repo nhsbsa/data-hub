db6f0b596165de0d43441c9871f2ac0fec963529
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2025-07-21 14:20:28 +0100

    Fixed routing

--- app/views/data-hub/mvp/v4/_routes.js
+++ app/views/data-hub/mvp/v4/_routes.js
@@ -1,29 +1,20 @@
 // External dependencies
 const express = require('express');
-
 const router = express.Router();
 
-// Sign in
-router.post(/sign-in/, (req, res) => {
+// ===================================
+// Sign in and out routes
+// ===================================
+router.post('/auth/sign-in', (req, res) => {
     req.session.data['signedIn'] = 'yes'
-    req.session.data['your-email']
     res.redirect('my-account'); 
 });
-
-// Sign out
-router.post(/sign-out/, (req, res) => {
+router.post('/sign-out', (req, res) => {
     req.session.data['signedIn'] = 'no'
     req.session.data['userType'] = ''
     res.redirect('/data-hub/mvp/v4/homepage');
 });
-
-router.post('/epact-user-journey', (req, res) => {    
-    req.session.data['userType'] = 'epact'
-    res.redirect('/data-hub/mvp/v4/homepage');
-});
-
-
-
+// ===================================
 
 
 
