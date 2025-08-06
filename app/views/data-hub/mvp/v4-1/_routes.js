7f69e21afce9fb43fcf824a37f718de3da37b410
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2025-08-06 15:16:24 +0100

    Content Design changes

--- app/views/data-hub/mvp/v4-1/_routes.js
+++ app/views/data-hub/mvp/v4-1/_routes.js
@@ -15,7 +15,25 @@ router.post(/sign-out/, (req, res) => {
     res.redirect('/data-hub/mvp/v4-1/homepage');
 });
 // ===================================
-
-
+// Create an account routes
+// ===================================
+router.post(/full-name/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-1/auth/job-role');
+});
+router.post(/job-role/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-1/auth/associated-organisation');
+});
+router.post(/associated-organisation/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-1/auth/your-email');
+});
+router.post(/your-email/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-1/auth/check-your-answers');
+});
+router.post(/auth-code/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-1/auth/create-password');
+});
+router.post(/create-password/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-1/auth/my-account');
+});
 
 module.exports = router;
