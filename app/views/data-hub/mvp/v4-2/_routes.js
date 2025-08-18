cb3d3f53193b5575be06b7bb912fa192a2068939
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2025-08-18 16:13:56 +0100

    v4.2 Entra ID Sign in

---
+++ app/views/data-hub/mvp/v4-2/_routes.js
@@ -0,0 +1,45 @@
+// External dependencies
+const express = require('express');
+const router = express.Router();
+
+// ===================================
+// Sign in and out routes
+// ===================================
+router.post('/auth/sign-in', (req, res) => {
+    req.session.data['signedIn'] = 'yes'
+    res.redirect('my-account'); 
+});
+router.post(/sign-out/, (req, res) => {
+    req.session.data['signedIn'] = 'no'
+    req.session.data['userType'] = ''
+    res.redirect('/data-hub/mvp/v4-2/homepage');
+});
+// ===================================
+// Create an account routes
+// ===================================
+router.post(/full-name/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/job-role');
+});
+router.post(/job-role/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/associated-organisation');
+});
+router.post(/associated-organisation/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/your-email');
+});
+router.post(/your-email/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/check-your-answers');
+});
+router.post(/auth-code/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/create-password');
+});
+router.post(/create-password/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/my-account');
+});
+// ===================================
+// Entra ID (replica)
+// ===================================
+router.post(/entra-id-sign-in/, (req, res) => {
+    res.redirect('/data-hub/mvp/v4-2/auth/entra-id/enter-password');
+});
+
+module.exports = router;
