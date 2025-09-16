7e75d92ae17b32c49560c0c23123928febdc0935
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2025-09-16 12:33:09 +0100

    removed assumptions routes

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,7 @@ router.use('/data-hub/mvp/v3', require('./views/data-hub/mvp/v3/_routes'));
 router.use('/data-hub/mvp/v2', require('./views/data-hub/mvp/v2/_routes'));
 
 // Assumptions Folder
-router.use('/data-hub/assumptions/v1-2', require('./views/data-hub/assumptions/v1-2/_routes'));
-router.use('/data-hub/assumptions/v1-1', require('./views/data-hub/assumptions/v1-1/_routes'));
+// router.use('/data-hub/assumptions/v1-2', require('./views/data-hub/assumptions/v1-2/_routes'));
+// router.use('/data-hub/assumptions/v1-1', require('./views/data-hub/assumptions/v1-1/_routes'));
 
 module.exports = router;
